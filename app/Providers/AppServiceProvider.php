75c55a79d9ca5496dd66bc2ed2bac1f8b39087da
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2026-01-18 21:53:08 -0800

    ssl cert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -54,6 +54,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+                URL::forceScheme('https');
+            }
         Document::observe(DocumentObserver::class);
     }
 }
