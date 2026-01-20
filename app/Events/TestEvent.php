58d48a7de57b2b771659a3b95bec8a72bc69c2ce
Jeff Kaufman <jeffkaufman@kaufmaninternational.com>
2026-01-19 20:01:30 -0800

    testing3

--- app/Events/TestEvent.php
+++ app/Events/TestEvent.php
@@ -4,12 +4,12 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\Channel;
 
-class TestEvent implements ShouldBroadcast
+class TestEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
